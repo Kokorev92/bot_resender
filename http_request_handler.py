fbd8904a4369050177216f6644ff2b2ca67d0889
Kokorev92 <kokorev.yaroslav@gmail.com>
2023-04-19 09:53:19 +0300

    [wip]: add html form with POST request

--- http_request_handler.py
+++ http_request_handler.py
@@ -1,12 +1,16 @@
 import http.server
 import socketserver
 import urllib.parse as urlparse
+from io import BytesIO
+import telebot
+import json
+
 
 class Http_request_handler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
 
-        if(self.path == '/'):
+        if self.path == '/':
             with open('index.html', 'r') as f:
                 index = f.read()
 
@@ -17,3 +21,15 @@ class Http_request_handler(http.server.BaseHTTPRequestHandler):
 
         self.send_error(404, "NOT FOUND")
 
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        body = self.rfile.read(content_length)
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        print(body)
+        response = BytesIO()
+        response.write(b'This is POST request. ')
+        response.write(b'Received: ')
+        response.write(body)
+        self.wfile.write(response.getvalue())
